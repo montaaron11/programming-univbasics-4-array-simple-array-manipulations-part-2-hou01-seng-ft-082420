a3464c5ef73dea1b5c8cc9d26cfa4db3ddf22c71
Aaron Montgomery <montaaron11@gmail.com>
2020-07-23 23:11:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+require 'pry'
+def using_concat(my_favorite_things, more_favs)
+   my_favorite_things.concat(more_favs)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end 
+
+def using_delete(array, string)
+  array.delete("Steven") 
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(2)
+end 
